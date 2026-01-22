893a0f4047d65e231b4dced9c06c16c3efabe79a
Andy Wilkinson <andrew@meonvalleyweb.com>
2026-01-22 17:45:07 +0000

    Load env vars for local functions

--- netlify/functions/_shared/supabase.js
+++ netlify/functions/_shared/supabase.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
